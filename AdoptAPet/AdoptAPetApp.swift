22f29566f6a126b8462214038e27b0c5e4f48ed2
Dmitriy Shmilo <dmitriy.shmilo@gmail.com>
2021-08-19 21:19:16 +0300

    Initial Commit

---
+++ AdoptAPet/AdoptAPetApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdoptAPetApp.swift
+//  AdoptAPet
+//
+//  Created by Dmitriy Shmilo on 19.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct AdoptAPetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AdoptAPet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AdoptAPet
+//
+//  Created by Dmitriy Shmilo on 19.08.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
